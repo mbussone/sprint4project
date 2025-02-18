eb66548382f477e22116c0dd01a1a0052c9189bb
mbussone <michael.g.bussone@gmail.com>
2025-02-18 00:01:20 -0400

    'clean'

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 #Import libraries
 import streamlit as st
 import pandas as pd
+import numpy as np
 import plotly.express as px
+from scipy.stats import zscore
 
 # Load data
 df = pd.read_csv('vehicles_us.csv')
@@ -12,6 +14,28 @@ df['date_posted']=pd.to_datetime(df['date_posted'])
 #get manufacturer
 df['manufacturer'] = df['model'].apply(lambda x:x.split()[0])
 
+# filling missing model_year values:
+df['model_year'] = df.groupby('model')['model_year'].transform(lambda x: x.fillna(x.median()))
+
+# filling missing cylinders
+df['cylinders'] = df.groupby('model')['cylinders'].transform(lambda x: x.fillna(x.median()))
+
+# Fill missing odometer values
+df['odometer'] = df.groupby(['model_year', 'model'])['odometer'].transform(lambda x: x.fillna(x.median()))
+
+# If there are still missing values, fill with the overall median
+overall_median = df['odometer'].median()
+df['odometer'] = df['odometer'].fillna(overall_median)
+
+# Remove outliers based on Z-scores
+print(df.describe())
+
+df = df[(np.abs(zscore(df['price'])) < 3) & (np.abs(zscore(df['model_year'])) < 3)]
+print()
+print('Results:')
+print()
+# Verify results
+print(df.describe())
 
 # Title
 st.title("Car Sales Dashboard")
@@ -82,7 +106,7 @@ fig = px.histogram(df_filtered,
 st.write(fig)
 
 if st.checkbox('Influence of Color'):
-    st.header("Influence of Color", anchor=None, help=None, divider="blue")
+    st.header("Influence of Color")
     fig = px.histogram(df, x='paint_color', title='Color Distribution')
     st.plotly_chart(fig)
     fig_scatter = px.scatter(df, x='paint_color', y='price', title='Price vs Color')
@@ -91,5 +115,5 @@ if st.checkbox('Influence of Color'):
     st.plotly_chart(fig_scatter)
 
 if st.checkbox('Show raw data'):
-    st.header("Raw Data", anchor=None, help=None, divider="gray")
+    st.header("Raw Data")
     st.write(df)
